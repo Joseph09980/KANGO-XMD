bb579e4a9c6b77e1e00392341f2ae9d9cd54022f
Joseph09980 <168485900+Joseph09980@users.noreply.github.com>
2025-05-18 10:24:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
-//-------------------[ BOT SETTINGS ]------------------// 
+you//-------------------[ BOT SETTINGS ]------------------// 
 
-// @project_name : KANGO-XMD 
-// @author : Hector 
+// @project_name : @dmin_personal_assistant 
+// @author : Joseph 
 // @telegram : http://t.me/official_kango
 // @github : OfficialKango
 // @whatsapp : +233509977126
 
-//----------------------[ KANGO-XMD ]----------------------//
+//----------------------[ @dmin_personal_assistant ]----------------------//
 
 const fs = require('fs')
 const { color } = require('./lib/color')
@@ -20,7 +20,7 @@ global.SESSION_ID = process.env.SESSION_ID || 'KANGO~TNUyRYjK#lyO-yqrAjnLlsdZZvK
 
 //--------------------[ BOT NAME ]----------------------//
 
-global.botname = process.env.BOT_NAME || 'KANGO-XMD' 
+global.botname = process.env.BOT_NAME || '@dmin_personal_assistant' 
 
 //-----------------[ OWNER NUMBER ]------------------//
 
@@ -34,15 +34,15 @@ global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', 
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'Joseph' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "KANGO-XMD" 
+global.packname = process.env.STICKER_PACK_NAME || "@dmin_ai" 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
-global.author = process.env.STICKER_AUTHOR_NAME || "Hector" 
+global.author = process.env.STICKER_AUTHOR_NAME || "@dmin_ai" 
 
 //----------------[ GITHUB DATABASE ]-----------------//
 
@@ -55,15 +55,15 @@ global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
 
 //------------------[ WATERMARK ]--------------------//
 
-global.wm = process.env.GL_WM || "> ©KANGO-XMD"
+global.wm = process.env.GL_WM || "> @dmin_ai"
 
 //---------------------[ REPLIES ]-----------------------//
 
 global.mess = { 
   done: '*Done*', 
-  success: '©kango-xmd', 
-  owner: `*You don't have permission to use this command!*`, 
-  group: '*This feature becomes available when you use it in a group!*', 
+  success: '©@dmin_ai', 
+  owner: `*U no get permissions to use this command!*`, 
+  group: '*This feature go dey available when you use am for group!*', 
   admin: '*You’ll unlock this feature with me as an admin!*', 
   notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
 }
